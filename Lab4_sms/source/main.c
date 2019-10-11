4604b943ec8a6f060485b2c6631796b53efc8436
neha45556 <neha45556@gmail.com>
2019-10-11 15:36:38 -0700

    Update main.c

--- Lab4_sms/source/main.c
+++ Lab4_sms/source/main.c
@@ -14,10 +14,30 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    enum LedStates { init, wait1, wait2, on1, on2} LedState ;
 
     /* Insert your solution below */
     while (1) {
-
+        switch(LedState){
+            case init:
+                LedState = wait1;
+                break;
+            case wait1:
+                if((PINA & 0x01) == 1){
+                    LedState = on1;
+                }
+                else{
+                    LedState = wait1;
+                }
+                break;
+            case on1:
+                if((PINA & 0x01) == 0
+                
+                
+        
+        
     }
     return 1;
 }
