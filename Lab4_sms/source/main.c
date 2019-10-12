e520a443ce969a2d30cf8c2d0fdee771b4b57578
neha45556 <neha45556@gmail.com>
2019-10-11 19:15:40 -0700

    Update main.c

--- Lab4_sms/source/main.c
+++ Lab4_sms/source/main.c
@@ -12,72 +12,106 @@
 #include "simAVRHeader.h"
 #endif
 
-	enum STATES { START, INIT, INCREMENT, RESET, DECREMENT } state;
-	unsigned char holder = 0x00;
-
-void tick() {
-switch(state) {
-case START:
-state = INIT;
-break;
-case INIT:
-if (PINA == 0) {
-state = RESET;
-} else if (PINA == 1) {
-state = INCREMENT;
-} else if (PINA == 2) {
-state = DECREMENT;
-} else {
-state = INIT;
-}
-break;
-case INCREMENT:
-state = INIT;
-break;
-case RESET:
-state = INIT;
-break;
-case DECREMENT:
-state = INIT;
-break;
-}
-
-switch(state) {
-case START:
-break;
-case INIT:
-break;
-case INCREMENT:
-if (holder < 9) {
-holder++;
-}
-break;
-case RESET:
-holder = 0;
-break;
-case DECREMENT:
-if (holder > 0) {
-holder--;
-}
-break;
-}
 
 
-
-}
-
+ enum STATES { START, INIT, STEP_1, STEP_2, STEP_3, OPEN} state;
+	//unsigned char holder = 0x00;
+   
+    
+    void tick(){
+        
+    switch(state) {
+    case START:
+    state = INIT;
+    break;
+            
+        case INIT:
+            if( PINA == 1){
+                state= STEP_1;
+            }
+            else{
+                state= INIT;
+            }
+           break;
+            
+        case STEP_1:
+          if(PINA == 1){
+              state= STEP_1;
+          }
+            else if(PINA == 0){
+                state= STEP_2;
+            }
+            else{
+                state= INIT;
+            }
+            break;
+            
+        case STEP_2:
+            if (PINA == 0){
+                state= STEP_2;
+            }
+            else if(PINA == 2){
+                state =STEP_3;
+            }
+            else {
+                state= INIT;
+            }
+            break;
+            
+        case STEP_3:
+            if ( PINA == 2){
+                state = STEP_3;
+            }
+            else if (PINA == 0){
+                state= OPEN;
+            }
+            else{
+                state = INIT;
+            }
+            break;
+            
+        case OPEN:
+            if ( PINA == 0x80){
+                state= INIT;
+            }
+            else{
+                state = OPEN;
+            }
+            break;
+    }
+        
+    switch(state) {
+    case START:
+    break;
+    case INIT:
+            PORTB= 0x00;
+    break;
+    
+        case STEP_1:
+            break;
+        case STEP_2:
+            break;
+        case STEP_3:
+            break;
+        case OPEN:
+            PORTB=0x01;
+            break;
+            
+    }
+    }    
 int main(void) {
     /* Insert DDR and PORT initializations */
-DDRA = 0x00; DDRC = 0xFF; PORTA = 0xFF; PORTC = 0x00;
-state = START;
-holder = 7;
+DDRA= 0xFF; PORTA=0x00;
+DDRB= 0x00; PORTB = 0xFF;
+    
+    /* Insert your solution below */
+   
+                
+    
     while (1) {
-//	holder = 7;
-	tick();
-	PORTC = holder;
+            tick();
     }
     return 1;
 }
-
 
 
