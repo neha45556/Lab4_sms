84328570f8837be48d0d3188d35b232f6fc3cdad
neha45556 <neha45556@gmail.com>
2019-10-11 20:25:04 -0700

    Update main.c

--- Lab4_sms/source/main.c
+++ Lab4_sms/source/main.c
@@ -12,106 +12,88 @@
 #include "simAVRHeader.h"
 #endif
 
-
-
- enum STATES { START, INIT, STEP_1, STEP_2, STEP_3, OPEN} state;
-	//unsigned char holder = 0x00;
-   
-    
-    void tick(){
-        
-    switch(state) {
-    case START:
-    state = INIT;
-    break;
-            
-        case INIT:
-            if( PINA == 1){
-                state= STEP_1;
-            }
-            else{
-                state= INIT;
-            }
-           break;
-            
-        case STEP_1:
-          if(PINA == 1){
-              state= STEP_1;
-          }
-            else if(PINA == 0){
-                state= STEP_2;
-            }
-            else{
-                state= INIT;
-            }
-            break;
-            
-        case STEP_2:
-            if (PINA == 0){
-                state= STEP_2;
-            }
-            else if(PINA == 2){
-                state =STEP_3;
-            }
-            else {
-                state= INIT;
-            }
-            break;
-            
-        case STEP_3:
-            if ( PINA == 2){
-                state = STEP_3;
-            }
-            else if (PINA == 0){
-                state= OPEN;
-            }
-            else{
-                state = INIT;
-            }
-            break;
-            
-        case OPEN:
-            if ( PINA == 0x80){
-                state= INIT;
-            }
-            else{
-                state = OPEN;
-            }
-            break;
-    }
-        
-    switch(state) {
-    case START:
-    break;
-    case INIT:
-            PORTB= 0x00;
-    break;
-    
-        case STEP_1:
-            break;
-        case STEP_2:
-            break;
-        case STEP_3:
-            break;
-        case OPEN:
-            PORTB=0x01;
-            break;
-            
-    }
-    }    
 int main(void) {
     /* Insert DDR and PORT initializations */
-DDRA= 0xFF; PORTA=0x00;
-DDRB= 0x00; PORTB = 0xFF;
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
     
+    enum states {INIT, WAIT, PRESS_P, RELEASE_P, PRESS_Y, OPEN} state;
+    state = INIT;
     /* Insert your solution below */
-   
-                
-    
     while (1) {
-            tick();
+        switch(state){
+            case INIT:
+                state = WAIT;
+                break;
+            case WAIT:
+                if(PINA == 4){
+                    state = PRESS_P;   
+                }
+                else{
+                    state = WAIT;    
+                }
+                break;
+            case PRESS_P:
+                if(PINA == 4){
+                    state = PRESS_P;   
+                }
+                else if(PINA == 0){
+                    state = RELEASE_P;    
+                }
+                else{
+                    state = WAIT;    
+                }
+                break;
+            case RELEASE_P:
+                if(PINA == 0){
+                    state = RELEASE_P;   
+                }
+                else if(PINA == 2){
+                    state = PRESS_Y;    
+                }
+                else{
+                    state = WAIT;    
+                }
+                break;
+            case PRESS_Y:
+                if(PINA == 2){
+                    state = OPEN;   
+                }
+                else if(PINA == 0){
+                    state = OPEN;    
+                }
+                else{
+                    state = WAIT;    
+                }
+                break;
+            case OPEN:
+                if(PINA == 0x80){
+                    state = WAIT;
+                }
+                else{
+                    state = OPEN;
+                }
+                break;
+
+        }   
+        switch(state){
+            case INIT:
+                PORTB = 0x00;
+                break;
+            case WAIT:
+                PORTB = 0x00;
+                break;
+            case PRESS_P:
+                break;
+            case RELEASE_P:
+                break;
+            case PRESS_Y:
+                break;
+            case OPEN:
+                PORTB = 0x01;
+                break;
+        }
     }
     return 1;
 }
-
 
